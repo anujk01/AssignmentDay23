73106d5090a8e1d82a527ba7c2f0e2edfb5d6c4b
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-03 22:30:53 +0530

    AddressBook

---
+++ UC3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AssignmentDay23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AssignmentDay23.UC3.Customer();
+            AssignmentDay23.UC3.Modify();
+            AssignmentDay23.UC3.Listing();
+
+        }
+    }
+}
+
+ 
