1883d7e9b0dbd989a9eb7c197779bde0dcff4bbe
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-03 22:34:38 +0530

    AddressBook

---
+++ UC4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AssignmentDay23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book System!");
+            Console.WriteLine("1.Enter to add the details");
+            Console.WriteLine("2.Enter to modify the details");
+            Console.WriteLine("3.Listing the details..");
+            Console.WriteLine("4.Remove the details");
+            Console.WriteLine("Enter a option");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    AssignmentDay23.UC4.Customer();
+                    AssignmentDay23.UC4.Listing();
+                    break;
+                case "2":
+                    AssignmentDay23.UC4.Customer();
+                    AssignmentDay23.UC4.Modify();
+                    AssignmentDay23.UC4.Listing();
+                    break;
+                case "3":
+                    AssignmentDay23.UC4.Customer();
+                    AssignmentDay23.UC4.Listing();
+                    break;
+                case "4":
+                    AssignmentDay23.UC4.Customer();
+                    AssignmentDay23.UC4.RemovePeople();
+
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+            }
+        }
+    }
+}
+
+ 
