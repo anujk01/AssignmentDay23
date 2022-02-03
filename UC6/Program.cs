44e1804d98b14ba6119977ebb6b286d44e8ebdfc
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-03 22:38:08 +0530

    AddressBook

---
+++ UC6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AssignmentDay23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book System!");
+            Console.WriteLine("1.Enter to add the details");
+            Console.WriteLine("2.Enter to modify the details");
+            Console.WriteLine("3.Listing the details..");
+            Console.WriteLine("4.Remove the details");
+            Console.WriteLine("Enter a option");
+            UC6 addressbook = new UC6();
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    addressbook.Customer();
+                    addressbook.Listing();
+                    break;
+                case "2":
+                    addressbook.Customer();
+                    addressbook.Modify();
+                    addressbook.Listing();
+                    break;
+                case "3":
+                    addressbook.Customer();
+                    addressbook.Listing();
+                    break;
+                case "4":
+                    addressbook.Customer();
+                    addressbook.RemovePeople();
+
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+            }
+
+        }
+    }
+}
+
+ 
